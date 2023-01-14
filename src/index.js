27dd0c5dc6b2b9905063b0c718d2f6547ca66da6
princekumarpaswan <prasantandal123@gmail.com>
2023-01-14 20:03:09 +0530

    added .env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom"
+import axios from 'axios';
 import App from "./App"
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
