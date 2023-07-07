9226361f560c8e41a2448b1255cccaf2201a186c
Prince Kumar paswan <43513990+princekumarpaswan@users.noreply.github.com>
2023-07-07 23:54:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 import axios from 'axios';
 import App from "./App";
 //import Auth from "./Auth";
-
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
